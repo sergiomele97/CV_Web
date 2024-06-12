ab8f73ea34df85e8ae4d60a06cf337f3df06b1eb
Sergio Melero <sergiomele97@gmail.com>
2024-06-12 14:03:57 +0200

    Ejemplo js

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,17 @@ function display_conocimientos(tecnologia){
 	
     break;
 
+// Perl ------------------------------//
+		case "Perl":
+		displayed_html = 
+	
+"# <a href='https://github.com/sergiomele97/Apuntes_Perl_Biotecnologia/blob/main/Algoritmos_geneticos/AlgoritmoGenetico.pl'>Desarrollo de algoritmos genéticos</a>"
++ "<br># <a href='https://github.com/sergiomele97/Apuntes_Perl_Biotecnologia/blob/main/Redes_neuronales/perceptron_simple.pl'>Modelización de bajo nivel de redes neuronales simples</a>"
++ "<br># <a href='https://github.com/sergiomele97/Apuntes_Perl_Biotecnologia/blob/main/Bioinformatica/Ejercicio%208.cgi'>Procesamiento de información biológica</a>"
++ "<br># <a>Desarrollo de aplicaciones de consola</a>"
++ "<br># <a>Implementación de herramientas bioinformáticas</a>";	
+    break;
+
 // Default ---------------------------//
 		default:
     // code block
